986fe0ba34acd5d6eaa784c7cd9c08c836180228
Sachin Garg <sachingarg.hcl@gmail.com>
2019-05-01 00:32:35 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.js';
+//import App from './Api.js';
+
+
+//ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
